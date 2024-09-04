e43e704a786d16f492518daf3ca0f47c0eeff1e4
abhidevfullstack <abhishekpurohit8989@gmail.com>
2024-09-04 16:15:48 +0000

    corrected code

--- models/index.js
+++ models/index.js
@@ -1,48 +1,74 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const basename = path.basename(__filename);
+// const config = require('../config/db.config.js');
+
+// const db = {};
+
+// // Initialize Sequelize
+// const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+//   host: config.HOST,
+//   dialect: config.dialect,
+//   operatorsAliases: 0,
+//   pool: {
+//     max: config.pool.max,
+//     min: config.pool.min,
+//     acquire: config.pool.acquire,
+//     idle: config.pool.idle
+//   }
+// });
+
+// // Dynamically import all models
+// fs.readdirSync(__dirname)
+//   .filter(file => {
+//     return (
+//       file.indexOf('.') !== 0 &&
+//       file !== basename &&
+//       file.slice(-3) === '.js' &&
+//       file.indexOf('.test.js') === -1
+//     );
+//   })
+//   .forEach(file => {
+//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//     db[model.name] = model;
+//   });
+
+// // Set up model associations if any
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// // Export Sequelize and models
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
 const config = require('../config/db.config.js');
 
-const db = {};
-
-// Initialize Sequelize
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   host: config.HOST,
   dialect: config.dialect,
-  operatorsAliases: 0,
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle
-  }
 });
 
-// Dynamically import all models
-fs.readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+const User = require('./user.model.js')(sequelize, Sequelize.DataTypes);
 
-// Set up model associations if any
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const Income = require('./income.model.js')(sequelize, Sequelize.DataTypes);
 
-// Export Sequelize and models
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+const Expense = require('./expense.model.js')(sequelize, Sequelize.DataTypes);
+
+
+
+const db = {
+  User,
+  Income,
+  Expense,
+
+  sequelize,
+  Sequelize,
+};
 
 module.exports = db;
